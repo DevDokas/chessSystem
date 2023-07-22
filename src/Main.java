3b42f9ebecc8eb92f9c05e1bef732496a7d37146
Dokas <iguinhogms@gmail.com>
2023-07-22 18:37:15 -0300

    board

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Main {
     public static void main(String[] args) {
 
-        Position position = new Position(3, 5);
-        System.out.println(position);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
