b7d76fd88a6109fb3873f9c58fb5fb86ab32be5f
Dokas <iguinhogms@gmail.com>
2023-07-23 00:46:01 -0300

    moves

--- src/UI.java
+++ src/UI.java
@@ -1,5 +1,9 @@
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class UI {
 
@@ -21,6 +25,17 @@ public class UI {
     public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+    public static ChessPosition readChessPosition(Scanner sc) {
+        try {
+            String s = sc.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+            return new ChessPosition(column, row);
+        } catch (RuntimeException e) {
+            throw  new InputMismatchException("Erro lendo ChessPosition. Posições válidas vão de a1 até h8.");
+        }
+    }
 
     public static void printBoard(ChessPiece[][] pieces) {
     for (int i = 0; i < pieces.length; i++) {
